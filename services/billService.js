1ef85ae42f1dbaafa9c87d5a242b6cc1f7096361
Prashanth Mogali <m.prashanth54@gmail.com>
2019-04-05 08:55:11 +0530

    Prashanth | added transactions to query

--- services/billService.js
+++ services/billService.js
@@ -1,11 +1,11 @@
 const knex = require('../db')
 
-exports.createBill = async (username, totalPrice, status) => {
+exports.createBill = async (username, totalPrice, status, trx) => {
   return await knex.returning(['id', 'total_price', 'status']).insert(
     {
       username: username,
       total_price: totalPrice,
       status: status
     }
-  ).table('bills')
+  ).table('bills').transacting(trx)
 }
--- services/salesService.js
+++ services/salesService.js
@@ -1,21 +1,21 @@
 const knex = require('../db')
 const billService = require('./billService')
 
-exports.getQueriesProductQty = (products) => {
+exports.getQueriesProductQty = (products, trx) => {
   return products.map((product) => {
     return knex('products').where({ id: product.id }).decrement({
       qty: product.qty,
-    })
+    }).transacting(trx)
   })
 }
 
-exports.insertSales = async (billID, products) => {
+exports.insertSales = async (billID, products, trx) => {
   const queries = products.map((product) => {
     return knex.insert({
       product: product.id,
       qty: product.qty,
       bill_no: billID
-    }).table('sales')
+    }).table('sales').transacting(trx)
   })
   return await Promise.all(queries)
 }
@@ -34,15 +34,18 @@ exports.calculatePrice = (products, productData) => {
 
 
 exports.generateBill = async (userID, products) => {
+  const trx = knex.transaction()
   try {
     const ids = products.map((product) => { return product.id })
     const productData = await knex.select('*').from('products').whereIn('id', ids)
-    await Promise.all(this.getQueriesProductQty(products))
+    await Promise.all(this.getQueriesProductQty(products, trx))
     const totalPrice = this.calculatePrice(products, productData)
-    let [bill] = await billService.createBill(userID, totalPrice, true)
-    await this.insertSales(bill.id, products)
+    let [bill] = await billService.createBill(userID, totalPrice, true, trx)
+    await this.insertSales(bill.id, products, trx)
+    trx.commit()
     return bill
   } catch (err) {
+    trx.rollback()
     console.log(err)
     throw `Unable to create the bill`
   }
